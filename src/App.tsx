3ed6e6f2b8c618fea9cf049afc56229fda45e6f7
Luis Paulo M. Degini <luispaulo.degini@gmail.com>
2020-07-04 10:10:21 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyles'
+
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <>
+    <Layout />
+    <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container, Tittle, ExpandIcon } from './styles'
+
+const ServerName: React.FC = () => {
+    return (
+        <Container>
+            <Tittle>Servidor do Hitk</Tittle>
+
+            <ExpandIcon />
+        </Container>
+    )
+}
+
+export default ServerName
